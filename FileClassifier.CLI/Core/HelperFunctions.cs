c41319a413d4278de7b07e3f079b2fdc8ad64223
Kovacevic22 <vukasinkue03@gmail.com>
2026-07-02 20:16:22 +0200

    Update ShowHelp function in HelperFunction
    Add explanation on how to scan a specific folder

--- FileClassifier.CLI/Core/HelperFunctions.cs
+++ FileClassifier.CLI/Core/HelperFunctions.cs
@@ -54,17 +54,20 @@ public static class HelperFunctions
         Console.WriteLine("=== File Classifier CLI ===");
         Console.ResetColor();
         Console.WriteLine("Usage:");
-        Console.WriteLine("  -s,  --scan         [Optional: path]  Scan and list files without moving them.");
-        Console.WriteLine("  -d,  --destination  <Required: path>  Set the target directory where folders will be created.");
-        Console.WriteLine("  -sr, --source       [Optional: path]  Set the custom directory to start organizing files from.");
+        Console.WriteLine("  -s,  --scan                            Scan and list files, without moving them.");
+        Console.WriteLine("  -s  -sr <path>                         Scan a specific folder without moving files.");
+        Console.WriteLine("  -d,  --destination  <Required: path>   Set the target directory where folders will be created.");
+        Console.WriteLine("  -sr, --source       [Optional: path]   Set the custom directory to start organizing files from.");
         if (OperatingSystem.IsWindows())
         {
             Console.WriteLine("\nExamples (Windows):");
+            Console.WriteLine(@"FileClassifier.CLI.exe -s -sr C:\Downloads");
             Console.WriteLine(@"FileClassifier.CLI.exe -d C:\Organized -sr C:\Downloads");
         }
         else
         {
             Console.WriteLine("\nExamples (Linux/macOS):");
+            Console.WriteLine("./FileClassifier.CLI -s -sr /home/user/Downloads");
             Console.WriteLine("./FileClassifier.CLI -d /home/user/Organized -sr /home/user/Downloads");
         }
     }
